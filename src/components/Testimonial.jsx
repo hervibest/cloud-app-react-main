8b45022732343105c43b809a40a326e4fa8c149b
hervibest <71449881+hervibest@users.noreply.github.com>
2022-07-21 21:22:31 +0700

    Initial commit

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { PhoneIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
+import {ChipIcon, SupportIcon} from '@heroicons/react/solid'
+
+import supportImg from '../assets/test.jpg'
+
+const Testimonial = () => {
+  return (
+  <div name='support' className='w-full mt-24'>
+      
+      
+      <div className='max-w-[1240px] mx-auto text-white relative'>
+          <div className='px-4 py-12'>
+              <h2 className='text-5xl pt-8 font-bold text-black text-center'>Testimonial</h2>
+          </div>
+
+          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+              <div className='bg-white justify-between items-center rounded-xl shadow-2xl'>
+                  <div className='p-8 block mx-auto relative justify-between items-center text-center'>
+                      <img src={supportImg} className="lg:max-w-[100px]  max-w-[250px] mx-auto items-center justify-between h-auto  relative"   >
+                      </img>
+                      <h3 className='font-bold text-2xl text-center my-6'>Sales</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
+                  </div>
+              </div>
+              <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                      <SupportIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+                      <h3 className='font-bold text-2xl my-6'>Technical Support</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
+                  </div>
+              </div>
+              <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                      <ChipIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+                      <h3 className='font-bold text-2xl my-6'>Media Inquiries</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  );
+};
+
+export default Testimonial;
